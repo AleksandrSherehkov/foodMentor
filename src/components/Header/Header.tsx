13b56b5217dbe3e4f557ffca1668e90b2ec8bb93
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2024-01-20 16:36:25 +0200

    Add:logo header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-// import { ReactComponent as Avocado } from '../../assets/avocado.svg&?react';
+import { IoIosArrowBack } from 'react-icons/io';
 
 interface HeaderProps {
   onBack: () => void;
@@ -8,14 +8,17 @@ interface HeaderProps {
 
 export const Header: FC<HeaderProps> = ({ onBack, canGoBack }) => {
   return (
-    <header className="w-full flex justify-center items-center py-2 px-5">
+    <header className="w-full flex justify-center items-center gap-x-4 py-2 px-5">
       {canGoBack && (
-        <button type="button" onClick={onBack}>
-          Back
+        <button
+          type="button"
+          onClick={onBack}
+          className="hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-300 active:bg-gray-300 transition duration-200 ease-in-out transform hover:scale-105"
+        >
+          <IoIosArrowBack size={26} />
         </button>
       )}
-      {/* <Avocado /> */}
-
+      <img src="/icons/avocado.svg" alt="Avocado" width="19" height="25" />
       <p className="text-2xl font-bold leading-9 tracking-[0.25px] text-depGray">Food Mentor</p>
     </header>
   );
